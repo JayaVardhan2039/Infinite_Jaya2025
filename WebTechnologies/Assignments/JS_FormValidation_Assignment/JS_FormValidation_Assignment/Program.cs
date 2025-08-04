ead3ccff5da3eb1bfb8d667adbc0c16b28566e05
JAYAVARDHAN GOVVADA <jayavardhang@infinite.com>
2025-08-04 18:19:36 +0530

    JS_FormValidation_Assignment

---
+++ WebTechnologies/Assignments/JS_FormValidation_Assignment/JS_FormValidation_Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JS_FormValidation_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
